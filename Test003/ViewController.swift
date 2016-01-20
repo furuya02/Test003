d58c85b7855547671a264360b182f078cc305fd5
平内真一 <hirauchi.shinichi@classmethod.jp>
2016-01-20 09:03:59 +0900

    Initial Commit

---
+++ Test003/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test003
+//
+//  Created by hirauchi.shinichi on 2016/01/20.
+//  Copyright © 2016年 underscore. inc underscore. inc underscore. inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
